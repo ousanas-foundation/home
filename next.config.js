2ca33c38ec8c4d34e9e379ace49d84047fce76b7
HabeshWallet <habeshawallet@gmail.com>
2022-06-08 17:14:53 +0200

    nojekll

--- next.config.js
+++ next.config.js
@@ -2,19 +2,21 @@ const withImages = require('next-images')
 const withCSS = require('@zeit/next-css')
 module.exports = withImages(withCSS())
 module.exports = withCSS({
-    webpack: function (config) {
-      config.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|jpeg|gif|ico)$/,
-        // test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-            name: '[name].[ext]'
-          }
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|jpeg|gif|ico)$/,
+      // test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]'
         }
       }
-      )
-      return config
     }
+    )
+    return config
+  }
 });
